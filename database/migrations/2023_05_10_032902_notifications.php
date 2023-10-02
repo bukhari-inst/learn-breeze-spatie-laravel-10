643a6d50ff2041c40232957fa4a96bfa0bc0a2a6
Bukhari_Inst <bukhari414647@gmail.com>
2023-10-02 13:57:38 +0700

    spatie

---
+++ database/migrations/2023_05_10_032902_notifications.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('message', 255);
+            $table->string('slug', 255);
+            $table->integer('status');
+            $table->string('created_by', 45);
+            $table->string('updated_by', 45);
+            $table->timestamp('read_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
